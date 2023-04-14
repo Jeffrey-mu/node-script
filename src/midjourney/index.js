0bb783120c99eacc1b6f6e01ea0e894e7f6fb162
Jeffrey <1115378579@qq.com>
2023-04-14 16:28:33 +0800

    feat: 添加爬取midjourney脚本

---
+++ src/midjourney/index.js
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import https from 'https';
+
+let data = fs.readFileSync('./index.txt').toString()
+const regex = /src\s*=\s*['"]?([^'"\s>]+)['"]?/g;
+let img_array = data.match(regex).map(item => item.split('"')[1].split('?')[0])
+let timestamp = `${+new Date()}/`
+
+img_array.forEach((item) => {
+  saveImg(item)
+})
+
+function saveImg(imageUrl) {
+  const filename = getFileName(imageUrl)
+  https.get(imageUrl, (response) => {
+    if (createFolderIfNotExists('./images/' + timestamp)) return
+    const fileStream = fs.createWriteStream('./images/' + timestamp + filename);
+    response.pipe(fileStream);
+    fileStream.on("finish", () => {
+      console.log(`File saved as ${filename}`);
+    });
+  }).on("error", (error) => {
+    console.error(error.message);
+  });
+}
+
+function getFileName(imageUrl) {
+  return imageUrl.split('/').at(-1)
+}
+
+function createFolderIfNotExists(folderPath) {
+  try {
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+  } catch (err) {
+    return true
+  }
+  return false
+}
