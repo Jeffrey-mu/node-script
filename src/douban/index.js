972bbcd47fcf68509d867b5fc369ee489d5333f3
Jeffrey <1115378579@qq.com>
2023-04-27 10:56:03 +0800

    chore: 修改文件夹

--- src/douban/index.js
+++ src/douban/index.js
@@ -4,28 +4,27 @@ import fs from 'fs'
 
 function readMovies() {
   // excel文件类径
-  const excelFilePath = './信息.xlsx'
+  const excelFilePath = './py/信息.xlsx'
 
   //解析excel, 获取到所有sheets
   const sheets = xlsx.parse(excelFilePath);
 
   // 查看页面数
-  console.log(sheets.length);
 
-
-  fs.writeFileSync('./movies.json', JSON.stringify(sheet.data.slice(1).map(item => ({ name: item[0], score: item[2], des: item[3], director: item[1] }))))
+  fs.writeFileSync('./json/movies.json', JSON.stringify(sheet.data.slice(1).map(item => ({ name: item[0], score: item[2], des: item[3], director: item[1] }))))
 
 }
 function readBooks() {
 
   const results = [];
 
-  fs.createReadStream('./doupanbooktest02.csv')
+  fs.createReadStream('./py/doupanbooktest02.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
       console.log();
-      fs.writeFileSync('./books.json', JSON.stringify(results))
+      fs.writeFileSync('./json/books.json', JSON.stringify(results))
     });
 }
-readBooks()
+// readBooks()
+// readMovies()
