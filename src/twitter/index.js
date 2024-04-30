62d9a69699588924321738e6effbee1c21ac4c4b
Jeffrey <1115378579@qq.com>
2024-04-30 10:07:19 +0800

    wip: *

--- src/twitter/index.js
+++ src/twitter/index.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs'
 import axios from 'axios'
 
-axios.get('https://twitter.com/vikingmute/status/1560447512561033217').then((res) => {
-  console.log(res)
-})
+axios.get('https://twitter.com/vikingmute/status/1560447512561033217')
+  .then((res) => {
+    fs.writeFile('data.txt', JSON.stringify(res.data), 'utf8', (err) => {
+      if (err)
+        throw err
+      else
+        console.log('Data has been written to data.txt')
+    })
+  })
+  .catch((error) => {
+    console.error('Error fetching the Twitter status:', error)
+  })
